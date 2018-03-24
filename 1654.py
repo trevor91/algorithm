4305319945e1c361ddecf8f4d5d8f62905843afb
Jehyung Lee <trevor91@naver.com>
2018-03-24 19:46:04 +0900

    .

--- 1654.py
+++ 1654.py
@@ -3,52 +3,26 @@ read = lambda: sys.stdin.readline()
 sys.setrecursionlimit(100000)
 
 def binary(start, end):
-	global right, rst
 	mid = (start+end) // 2
-	if mid == 0 or start == end:
-		rst = end
+	if end - start < 2:
+		rst1 = sum(map(lambda x:x//mid, cables))
+		rst2 = sum(map(lambda x:x//(mid-1), cables))
+		if rst1 >= n:
+			print(mid)
+		else:
+			print(mid-1)
 		return()
 	cnt = sum(map(lambda x:x//mid, cables))
-	if cnt > n:
-		right = end
+	if cnt >= n:
 		binary(mid+1, end)
 	elif cnt < n:
 		binary(start,mid)
-	else:
-		rst = mid
-
-def binary2(start,end):
-	global rst, right
-	mid = (start+end)//2
-	if mid == 0 or start == end: return()
-	cnt = sum(map(lambda x:x//mid, cables))
-	if cnt == n:
-		rst = mid
-		binary2(mid+1,end)
-	else:
-		right = end
-		binary2(start,mid)
-
+	
 if __name__ == '__main__':
 	k,n = map(int,read().split())
 	cables = [int(read()) for _ in range(k)]
 	s = 0
-	right = 0
 	for i in cables:
 		s += i
-		if i > right:
-			right = i
 	cable_length = s // n
-	rst = 0
 	binary(0,cable_length+1)
-	binary2(rst,right)
-
-	for i in range(rst,right+1):
-		cnt = 0
-		for cable in cables:
-			cnt += cable // i
-		if cnt!=n:
-			print(i-1)
-			break
-	else:
-		print(i)
