e341132184d5eadc9737eb945152758b96eb1ca9
Jehyung Lee <trevor91@naver.com>
2018-04-29 19:21:35 +0900

    greedy

---
+++ beakjoon/1931.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(const pair<int,int> &v1, const pair<int,int> &v2)
+{
+	if(v1.second == v2.second)
+		return v1.first < v2.first;
+	return v1.second < v2.second;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	vector< pair<int, int> > v(n);
+	for(int i=0; i<n; i++)
+		scanf("%d %d", &v[i].first, &v[i].second);
+	sort(v.begin(), v.end(), cmp);
+	unsigned int start_define = 0;
+	unsigned int end_min = -1;
+	int rst = 0;
+
+	for(int i=0; i<n; i++)
+	{
+		if(v[i].first >= start_define)
+		{
+			if(v[i].second < end_min)
+			{
+				end_min = v[i].second;
+				rst++;
+				start_define = end_min;
+				end_min = -1;
+			}
+		}
+	}
+	printf("%d\n",rst);
+	return 0;
+}
