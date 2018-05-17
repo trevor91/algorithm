cf36435d721b6c743a5b0e784433520a0cbc26de
Jehyung Lee <trevor91@naver.com>
2018-05-17 18:10:41 +0900

    .mody.

--- beakjoon/1753.py
+++ beakjoon/1753.py
@@ -10,28 +10,28 @@ if __name__ == '__main__':
 	path = [[] for _ in range(v+1)]
 	memo = array('f',[math.inf]*(v+1))
 	memo[start] = 0
-	s = set()
+	s = array('b', [0]*(v+1))
 	s_q = queue.Queue()
-	q = list(range(1,v+1))
 
 	for _ in range(e):
 		temp_path = [int(x) for x in r()]
 		if temp_path[1] != start:
 			path[temp_path[0]].append([temp_path[1],temp_path[2]])
-	s.add(start)
+	s[start] = 1
 	s_q.put(start)
-	q.remove(start)
+
 	while not s_q.empty():
 		sub_start = s_q.get()
-		for target, weght in path[sub_start]:
-			if not target in s:
+		for target, w in path[sub_start]:
+			flag = 0
+			if s[target] == 0:
+				s[target] = 1
+				flag = 1
+			if memo[sub_start] + w < memo[target]:
+				memo[target] = memo[sub_start] + w
+				flag = 1
+			if flag:
 				s_q.put(target)
-				q.remove(target)
-				s.add(target)
-			if memo[sub_start] + weght < memo[target]:
-				memo[target] = memo[sub_start] + weght
-				s_q.put(target)
-
 	for i in range(1,len(memo)):
 		if memo[i] == math.inf:
 			print("INF")
