0cd8ca5b7b62836239ec43e956c5268aeeb1989f
Jehyung Lee <trevor91@naver.com>
2018-10-29 00:53:35 +0900

    add quick(arr) func

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -16,9 +16,26 @@ def quick(start, end):
     quick(s,start-1)
     quick(start+1, e)
 
+def quick2(a):
+    if len(a) < 2:
+        return a
+    start = 0
+    end = len(a)-1
+    pivot = a[(start + end) // 2]
+    while start < end:
+        if a[start] < pivot:
+            start+=1
+            continue
+        if a[end] > pivot:
+            end-=1
+            continue
+        a[start], a[end] = a[end], a[start]
+    return quick2(a[:start]) + [pivot] + quick2(a[start+1:])
+
 if __name__ == '__main__':
-    a = list(range(100))
+    a = list(range(5))
     random.shuffle(a)
     print(a)
-    quick(0, len(a)-1)
+    #quick(0, len(a)-1)
+    a = quick2(a)
     print(a)
